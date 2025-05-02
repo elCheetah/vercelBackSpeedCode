9697e464253e053d857b78977161612926b75458
Diego <diegodelgadillo2024@gmail.com>
2025-05-02 03:55:46 -0400

    cambios2

--- src/routes/filtroGPSPersonalizadaRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { buscarVehiculosCercanos } from '../controllers/filtroGPSPersonalizadaController';  // Correcto
-
-const router = Router();
-
-// GET /vehiculos/cercanos?lat=...&lon=...&radio=...
-router.get('/cercanos', buscarVehiculosCercanos);
-
-export default router;
